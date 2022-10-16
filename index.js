0e1738f97c4ba48e5520e99e2f56d5118dcf133e
yusuf1nb <ynbno.3@gmail.com>
2022-10-16 23:14:30 +0100

    advacded donee

---
+++ index.js
@@ -0,0 +1,64 @@
+// template_cp912xh
+// service_lllfq4g
+// SesNVFd5rjXKVMh0W
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    // Added rotate after tutorial
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${
+      y * boolInt
+    }px) rotate(${x * boolInt * 10}deg)`;
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_lllfq4g",
+      "template_cp912xh",
+      event.target,
+      "SesNVFd5rjXKVMh0W"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "this isnt working right now please email me at bakshiyusuf@gmail.com"
+      );
+    });
+}
+
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal__open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal__open";
+}
